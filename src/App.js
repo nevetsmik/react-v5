185bcb910982e759951cf84ceb6a0cd2443d4ad5
steve <emailstevenkim@gmail.com>
2020-07-27 14:21:32 -0400

    Pure React

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { id: "an-attribute" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Dog",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Siamese",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
